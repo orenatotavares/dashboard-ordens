ee5d662ef11cff285c5ed4726ed8f494c1fb1eb4
Renato <130257539+orenatotavares@users.noreply.github.com>
2025-05-22 21:20:43 -0300

    Delete pages/teste.py

--- pages/teste.py
+++
@@ -1,2 +0,0 @@
-df_hoje['closed_ts_dt'] = pd.to_datetime(df_hoje['closed_ts'], unit='ms', errors='coerce').dt.tz_localize('UTC').dt.tz_convert('America/Sao_Paulo')
-df_hoje = df_hoje[df_hoje['closed_ts_dt'].dt.date == data_hoje]
